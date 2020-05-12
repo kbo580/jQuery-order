42f9d6936185ac4b1dfad84f790648a721eee551
kbo580 <61081989+kbo580@users.noreply.github.com>
2020-05-12 22:27:02 +0900

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$('.text-change span').text()
+$('#get-text').text($('.text-change span').text()).css('color','red');
+//$('#get-html').html();
+$('#change-html').html('<strong>こんばんわ！</strong>').css('color','red');
+$('#change-html').html();
+$('#get-html').html($('#change-html').html()).css('color','red');
+
+//$('#change-Html').html('<strong>changed!</strong>');
+// $('body').css('backgroundColor');
+$('#insert>li:eq(0)').prepend('<strong>これがprepend</strong>');
+$('#insert>li:eq(3)').append('<strong>これがappend</strong>');
+$('#insert>li:eq(0)').before('<h2>これはbefore</h2>');
+$('#insert>li:eq(3)').after('<h2>これはafter</h2>');
+
+$('<strong>これがprependTo</strong>').prependTo('#insert-move>li:eq(0)').css('color','red');
+$('<strong>これがappendTo</strong>').appendTo('#insert-move>li:eq(3)').css('color','red');
+$('.insert-before').insertBefore('#insert-move>li:eq(0)');
+$('.insert-after').insertAfter('#insert-move>li:eq(3)');
+
+$('#change-attribute').attr('href','yahoo.co.jp').css('color','red');
+$('#get-attribute').text($('#google').attr('href')).css('color','red');
+$('#change-css').css('color','red');
+$('#change-css-three').css({'color':'red','backgroundColor':'yellow','display':'inline'});
+$('#change-css-three').css("backgroundColor");
+
+$('#main>li:eq(1)').next().css('color','green');
+$('#main>li:eq(2)').prev().css('color','skyblue');
+$('#sub>li:eq(1)').siblings().css('color','orange');
